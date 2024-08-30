cf931c22756969e148b8788f3e00f182a48052da
Matias Taiana <matiastaiana@hotmail.com>
2024-08-30 19:28:22 -0300

    correccion de titulo

--- js/app.js
+++ js/app.js
@@ -65,3 +65,18 @@ document.addEventListener('scroll', function() {
         document.querySelector('.titulo').style.marginTop = '120px'; // Ajusta según el espacio necesario
     }
 });
+document.addEventListener('scroll', function() {
+    const scrollTop = window.scrollY;
+    const titulo = document.getElementById('titulo');
+    const contenedorTitulos = document.querySelector('.contenedorTitulos');
+
+    if (scrollTop > 50) { // Ajusta este valor según tus preferencias
+        titulo.classList.add('titulo-fixed');
+        contenedorTitulos.style.marginTop = '10px'; // Ajusta según el espacio necesario
+        contenedorTitulos.style.paddingTop = '0px'; // Reduce el padding-top en 120px al hacer scroll
+    } else {
+        titulo.classList.remove('titulo-fixed');
+        contenedorTitulos.style.marginTop = '0px'; // Ajusta según el espacio necesario
+        contenedorTitulos.style.paddingTop = '20px'; // Restaura el padding-top original
+    }
+});
