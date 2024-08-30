ff6934bc57a026b731acb81fd1f5f1ab747d802a
Matias Taiana <matiastaiana@hotmail.com>
2024-08-30 14:04:47 -0300

    titulo y logo

--- js/app.js
+++ js/app.js
@@ -37,3 +37,31 @@ document.querySelectorAll('.card').forEach(card => {
         }
     });
 });
+
+// Selecciona el contenedor del logo
+const logoContainer = document.querySelector('.contenedorLogo');
+const logo = document.querySelector('.logo');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        // Cuando se hace scroll hacia abajo
+        logo.classList.add('shrink-logo');
+        logoContainer.classList.add('shrink-logo');
+    } else {
+        // Cuando se vuelve al principio de la página
+        logo.classList.remove('shrink-logo');
+        logoContainer.classList.remove('shrink-logo');
+    }
+});
+document.addEventListener('scroll', function() {
+    const scrollTop = window.scrollY;
+    const titulo = document.getElementById('titulo');
+
+    if (scrollTop > 0) { // Ajusta este valor según tus preferencias
+        titulo.classList.add('titulo-fixed');
+        document.querySelector('.titulo').style.marginTop = '10px'; // Ajusta según el espacio necesario
+    } else {
+        titulo.classList.remove('titulo-fixed');
+        document.querySelector('.titulo').style.marginTop = '120px'; // Ajusta según el espacio necesario
+    }
+});
